d0823315a0830683f9b05766cfca7c844a246330
anand <anandanand99599@gmail.com>
2024-08-05 20:39:56 +0530

    giving permission to ci server and code dry

--- vars/mainCI.groovy
+++ vars/mainCI.groovy
@@ -1,11 +1,28 @@
 def call() {
   node ( 'ci-server' ) {
+
+    stage('CodeCheckout') {
+
+      sh "find ."
+      sh "find . | sed -e '1d' |xargs rm -rf"
+      if(env.TAG_NAME ==~ ".*") {
+        env.branch_name = "refs/tags/${env.TAG_NAME}"
+      } else {
+        env.branch_name = "${env.BRANCH_NAME}"
+      }
+      checkout scmGit(
+              branches: [[name: "${branch_name}"]],
+              userRemoteConfigs: [[url: "https://github.com/anand-320/expense-${component}"]]
+      )
+    }
+
     if (env.TAG_NAME ==~ ',*') {
       stage('Build Code'){
-        print 'ok'
+        sh 'docker build -t 942136769355.dkr.ecr.us-east-1.amazonaws.com/expense-${component}:${TAG_NAME} .'
       }
       stage('Release software'){
-        print 'ok'
+        sh 'aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 942136769355.dkr.ecr.us-east-1.amazonaws.com'
+        sh 'docker push 942136769355.dkr.ecr.us-east-1.amazonaws.com/expense-${component}:${TAG_NAME} '
       }
     } else {
       stage('lint code') {
